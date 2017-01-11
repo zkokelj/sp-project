9696374f00a34234070b2be9bc0dfd84a1332c2a
Ziga Kokelj <ziga.kokelj@gmail.com>
2017-01-11 12:49:22 +0000

    Unit test added

--- tests/UnitTest.php
+++ tests/UnitTest.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
+use App\User;
 
 class UnitTest extends TestCase
 {
@@ -14,5 +15,13 @@ class UnitTest extends TestCase
     public function testExample()
     {
         $this->assertTrue(true);
+    }
+
+
+    /** @test */
+    public function user_can_be_created(){
+      $user = factory(App\User::class)->create();
+      echo $user;
+      $this->seeInDatabase('users', ['id'=>$user['id'], 'name'=>$user['name'], 'email' =>$user['email']]);
     }
 }
